d860f2322180908922db204627efc8fc9fc0e7cc
widjesh <widjesh@satyamholidays.com>
2019-11-06 16:52:46 -0600

    Finished login

--- account-service/database/db.js
+++ account-service/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('node','root','root',{
+const connection = new Sequelize('node','node','root',{
     host : 'localhost',
     dialect : 'mysql',
     operatorAliases:false,
