e640bda9b3ba9d7b19aa39971fefdbccf10ab3b1
Nduwayo Fabrice <nduwayof@outlook.com>
2019-11-07 15:07:43 -0600

    Minor changes

---
+++ shipping-service/src/main/java/edu/mum/cs/shipping/service/ShippingServiceApplication.java
@@ -0,0 +1,21 @@
+package edu.mum.cs.shipping.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Shipping service application.
+ */
+@SpringBootApplication
+public class ShippingServiceApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingServiceApplication.class, args);
+    }
+
+}
---
+++ shipping-service/src/test/java/edu/mum/cs/shipping/service/ShippingServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package edu.mum.cs.shipping.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingServiceApplicationTests {
+}
